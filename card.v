49ad4da26356d49ada9a2d538572feabdc4e6a17
Chanzo Odane Bryan <bryanch3@IITS-I402-15.utsc-labs.utoronto.ca>
2019-03-25 18:35:30 -0400

    Fixed card commit

--- card.v
+++ card.v
@@ -4,45 +4,13 @@ module store_card(
 	input [1:0] suit, // The card suit
 	input [9:0] next_card, // The memory address of the next card
 	input clock, // Stores to memory on this clock tick
-
-	input enable, // If the module should store,
-	input load_val, // If the module should load the value and suit (takes priority over storing)
-	input load_addr, // If the module should load the memory address of the last and next card
-	output reg [9:0] last_card, // The memory address of the last card's next
-	output [7:0] card // The card at the current address of the module
-
 	input enable, // If the module should store
 	input load, // If the module should load the address, value and suit
 	output [31:0] card_data, // The data at the current address of the module
-
 	);
 
 	reg [3:0] current_value; // The current value of the module
 	reg [1:0] current_suit; // The current suit of the module
-
-	reg [9:0] current_next; // The next card
-
-	always @(posedge clock) begin
-		if(load_val) begin
-			current_value <= value;
-			current_suit <= suit;
-		end
-		else if(load_addr) begin
-			current_next <= next_card;
-		end
-		else if(enable) begin
-			last_card <= address;
-		end
-	end
-
-	ram1024x32 ram(
-		.address(address),
-		.clock(clock),
-		.data({suit, value}),
-		.wren(enable),
-		.q(card)
-	);
-endmodule
 	reg [9:0] current_addr; // The current address of the module
 	reg wren; // If the ram module should write
 
@@ -102,7 +70,7 @@ module remove_card(
  	reg [2:0] current_state;
 
  	initial begin
- 		empty_card = 32'b0;
+ 		empty_card = {1, 31'b0};
  	end
 
 	localparam LOAD_ADDRESS = 3'd0, // First load the address
@@ -200,3 +168,4 @@ module nth_card(
 		.q(next_card)
 	);
 endmodule
+
