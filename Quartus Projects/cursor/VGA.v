16f86253fff25ab4e683513fce6ddd6ae0422f5e
Anson Tran <trananso@IITS-I402-05.utsc-labs.utoronto.ca>
2019-03-28 21:07:58 -0400

    made 2 of hearts

--- Quartus Projects/cursor/VGA.v
+++ Quartus Projects/cursor/VGA.v
@@ -6,7 +6,7 @@ module cursor(
     KEY,
     SW,
     // The ports below are for the VGA output.  Do not change.
-    VGA_CLK,   				//	VGA Clock
+   	VGA_CLK,   				//	VGA Clock
     VGA_HS,					//	VGA H_SYNC
     VGA_VS,					//	VGA V_SYNC
     VGA_BLANK_N,			//	VGA BLANK
@@ -17,9 +17,9 @@ module cursor(
     HEX7,
     HEX6,
     HEX5,
-    HEX4,
-    HEX3,
-    LEDR
+   	HEX4,
+    	HEX3,
+    	LEDR
 	);
 
 	input	CLOCK_50;		//	50 MHz
@@ -27,22 +27,16 @@ module cursor(
 	input   [3:0]   KEY;
 	output  [17:0] LEDR;
 	
-
-	/**assign resetn = SW[10];
-	assign load_x = ~KEY[3];
-	assign go = ~KEY[2];
-	assign data_in = SW[9:0];*/
-	
 	// Declare your inputs and outputs here
 	// Do not change the following outputs
-	output	VGA_CLK;   				    //	VGA Clock
-	output	VGA_HS;					    //	VGA H_SYNC
-	output	VGA_VS;					    //	VGA V_SYNC
-	output	VGA_BLANK_N;				//	VGA BLANK
-	output	VGA_SYNC_N;				    //	VGA SYNC
-	output	[9:0]	VGA_R;   			//	VGA Red[9:0]
-	output	[9:0]	VGA_G;	 			//	VGA Green[9:0]
-	output	[9:0]	VGA_B;  			//	VGA Blue[9:0]
+	output	VGA_CLK;   		//	VGA Clock
+	output	VGA_HS;			//	VGA H_SYNC
+	output	VGA_VS;			//	VGA V_SYNC
+	output	VGA_BLANK_N;	//	VGA BLANK
+	output	VGA_SYNC_N;		//	VGA SYNC
+	output	[9:0] VGA_R;  //	VGA Red[9:0]
+	output	[9:0] VGA_G;	//	VGA Green[9:0]
+	output	[9:0] VGA_B;  //	VGA Blue[9:0]
 	
 	output [6:0] HEX7;
 	output [6:0] HEX6;
@@ -57,6 +51,7 @@ module cursor(
 	wire plot;
    wire resetN;
 	assign resetN = SW[16];
+	assign go = SW[17];
 
 	// Create an Instance of a VGA controller - there can be only one!
 	// Define the number of colours as well as the initial background
@@ -67,7 +62,7 @@ module cursor(
 			.colour(colour[2:0]),
 			.x(x[7:0]),
 			.y(y[6:0]),
-			.plot(plot),
+			.plot(1'b1),
 			/* Signals for the DAC to drive the monitor. */
 			.VGA_R(VGA_R),
 			.VGA_G(VGA_G),
@@ -97,14 +92,14 @@ module cursor(
 
 		.xIn(8'd30),                // 8 bit x location input
 		.yIn(7'd10),                // 7 bit y location input
-	
-		.loadNumSuit(loadNumSuit),
+
+		.loadNumSuit(loadNumSuit),	 // FSM signals
 		.drawBlank(drawBlank),
 		.drawNum(drawNum),
 		.drawSuit(drawSuit),
-	
+
 		.cardNum(4'd2),
-		.cardSuit(2'd4),
+		.cardSuit(2'd3),
 
 		.xOut(x[7:0]),
 		.yOut(y[6:0]),
@@ -117,9 +112,9 @@ module cursor(
 		
 	
 	control FSM(
-		.resetN(resetN),               // Resets the states
-		.clock(CLOCK_50),                // FSM clock
-		.go(SW[17]),                   // Begins FSM cycle
+		.resetN(resetN),            // Resets the states
+		.clock(CLOCK_50),           // FSM clock
+		.go(go),                    // Begins FSM cycle
 	 
 		.blankDone(blankDone),
 		.numDone(numDone),
@@ -134,6 +129,22 @@ module cursor(
 		.current_state(LEDR[17:13]),
 		.next_state(LEDR[12:8])
 	);
+	
+	
+	/**drawNumSuit num(
+		.resetN(resetN),
+		.clock(CLOCK_50),
+		.start(go),
+		.x(8'd30),
+		.y(7'd10),
+		.data(768'hFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFC0FFFFFFFFFE071FFFFFFFFE3F1FFFFFFFFFF8FFFFFFFFFE00FFFFFFFFFE3FFFFFFFFFFE001FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF),
+		.xOut(x[7:0]),
+		.yOut(y[6:0]),
+		.cOut(colour[2:0]),
+		.done()
+	);**/
+	
+	
 	
 	HexDecoder hexX1(.IN(x[7:4]),
 					.OUT(HEX7[6:0])
@@ -179,34 +190,35 @@ module drawCard(
 	output numDone,
 	output suitDone
 	);
-	 
-	wire [767:0] ace;
-	wire [767:0] two;
-	wire [767:0] three;
-	wire [767:0] four;
-	wire [767:0] five;
-	wire [767:0] six;
-	wire [767:0] seven;
-	wire [767:0] eight;
-	wire [767:0] nine;
-	wire [767:0] ten;
-	wire [767:0] jack;
-	wire [767:0] queen;
-	wire [767:0] king;
 	
-	wire [767:0] spade;
-	wire [767:0] diamond;
-	wire [767:0] club;
-	wire [767:0] heart;
-	assign two = 768'hFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFC0FFFFFFFFFE071FFFFFFFFE3F1FFFFFFFFFF8FFFFFFFFFE00FFFFFFFFFE3FFFFFFFFFFE001FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF;
-	assign heart = 768'hFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF03003FFFFFF8000007FFFFF8000007FFFFF8000007FFFFF8000007FFFFFF00003FFFFFFFE001FFFFFFFFFC7FFFFFFFFFFFBFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF;
-	
-	reg [767:0] numDataReg, suitDataReg;
 	reg [3:0] numReg;
 	reg [1:0] suitReg;
 	reg [7:0] xReg;
 	reg [6:0] yReg;
 	
+	reg [767:0] numDataReg, suitDataReg;
+	
+	
+	localparam
+		ACE = 768'h0,
+		TWO = 768'hFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFC0FFFFFFFFFE071FFFFFFFFE3F1FFFFFFFFFF8FFFFFFFFFE00FFFFFFFFFE3FFFFFFFFFFE001FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF,
+		THREE = 768'h0,
+		FOUR = 768'h0,	
+		FIVE = 768'h0,	
+		SIX = 768'h0,	
+		SEVEN = 768'h0,	
+		EIGHT = 768'h0,	
+		NINE = 768'h0,	
+		TEN = 768'h0,	
+		JACK = 768'h0,	
+		QUEEN = 768'h0,	
+		KING = 768'h0,	
+		SPADE = 768'h0,	
+		DIAMOND = 768'h0,	
+		CLUB = 768'h0,	
+		HEART = 768'hFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF03003FFFFFF8000007FFFFF8000007FFFFF8000007FFFFF8000007FFFFFF00003FFFFFFFE001FFFFFFFFFC7FFFFFFFFFFFBFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF;	
+
+		
 	always@(posedge clock) begin
 		if (!resetN) begin
 			numReg <= 4'b0;
@@ -218,29 +230,29 @@ module drawCard(
 	end
 	
 	always@(posedge clock) begin
-		case (numReg) 
-			4'd1: numDataReg <= ace;
-			4'd2: numDataReg <= two;
-			4'd3: numDataReg <= three;
-			4'd4: numDataReg <= four;
-			4'd5: numDataReg <= five;
-			4'd6: numDataReg <= six;
-			4'd7: numDataReg <= seven;
-			4'd8: numDataReg <= eight;
-			4'd9: numDataReg <= nine;
-			4'd10: numDataReg <= ten;
-			4'd11: numDataReg <= jack;
-			4'd12: numDataReg <= queen;
-			4'd13: numDataReg <= king;
-			default: numDataReg <= ace;
+		case (numReg)
+			4'd1: numDataReg <= ACE;
+			4'd2: numDataReg <= TWO;
+			4'd3: numDataReg <= THREE;
+			4'd4: numDataReg <= FOUR;
+			4'd5: numDataReg <= FIVE;
+			4'd6: numDataReg <= SIX;
+			4'd7: numDataReg <= SEVEN;
+			4'd8: numDataReg <= EIGHT;
+			4'd9: numDataReg <= NINE;
+			4'd10: numDataReg <= TEN;
+			4'd11: numDataReg <= JACK;
+			4'd12: numDataReg <= QUEEN;
+			4'd13: numDataReg <= KING;
+			default: numDataReg <= 768'b0;
 		endcase
 		
 		case (suitReg)
-			2'd0: suitDataReg <= spade;
-			2'd1: suitDataReg <= diamond;
-			2'd2: suitDataReg <= club;
-			2'd3: suitDataReg <= heart;
-			default: suitDataReg <= spade;
+			2'd0: suitDataReg <= SPADE;
+			2'd1: suitDataReg <= DIAMOND;
+			2'd2: suitDataReg <= CLUB;
+			2'd3: suitDataReg <= HEART;
+			default: suitDataReg <= 768'b0;
 		endcase
 	end
 	
@@ -251,6 +263,7 @@ module drawCard(
 	drawBlankCard blank(
 		.resetN(resetN),
 		.clock(clock),
+		.start(drawBlank),
 		.x(xIn),
 		.y(yIn),
 		.xOut(blankOutX),
@@ -260,25 +273,26 @@ module drawCard(
 	
 	);
 	
-	drawNum num(
+	drawNumSuit num(
 		.resetN(resetN),
 		.clock(clock),
-		.x(xIn),
-		.y(yIn),
-		.in_data(numReg),
+		.start(drawNum),
+		.x(xIn + 2'd2),
+		.y(yIn + 2'd2),
+		.data(numDataReg),
 		.xOut(numOutX),
 		.yOut(numOutY),
 		.cOut(numOutC),
 		.done(numDone)
 	);
 	
-	
-	drawNum suit(
+	drawNumSuit suit(
 		.resetN(resetN),
 		.clock(clock),
-		.x(xIn),
-		.y(yIn+5'd16),
-		.in_data(suitReg),
+		.start(drawSuit),
+		.x(xIn + 2'd2),
+		.y(yIn + 5'd16),
+		.data(suitDataReg),
 		.xOut(suitOutX),
 		.yOut(suitOutY),
 		.cOut(suitOutC),
@@ -312,80 +326,68 @@ module control(
     input clock,                // FSM clock
     input go,                   // Begins FSM cycle
 	 
-	 input blankDone,
-	 input numDone,
-	 input suitDone,
+	input blankDone,
+	input numDone,
+	input suitDone,
 
-	 output reg drawBlank,
-	 output reg drawNum,
-	 output reg drawSuit,
-	 output reg loadNumSuit,
-    output reg plot,            // Signal to output the current pixel and draw on the VGA monitor
-	 output reg [4:0] current_state, next_state
+	output reg drawBlank,
+	output reg drawNum,
+	output reg drawSuit,
+	output reg loadNumSuit,
+   output reg plot,            	// Signal to output the current pixel and draw on the VGA monitor
+	output reg [4:0] current_state, next_state
     );
 
     //reg [4:0] current_state, next_state;
 
     localparam
-        NO_DRAW      = 4'd0,
-		  LOAD_NUM_SUIT= 4'd1,
-        DRAW_BLANK  	= 4'd2,
-		  WAIT_BLANK  	= 4'd3,
-		  DRAW_NUM	  	= 4'd4,
-		  WAIT_NUM	  	= 4'd5,
-		  DRAW_SUIT   	= 4'd6,
-		  WAIT_SUIT	  	= 4'd7;
+			NO_DRAW      	= 4'd0,
+			LOAD_NUM_SUIT	= 4'd1,
+			DRAW_BLANK  	= 4'd2,
+			WAIT_BLANK  	= 4'd3,
+			DRAW_NUM			= 4'd4,
+			WAIT_NUM			= 4'd5,
+			DRAW_SUIT		= 4'd6,
+			WAIT_SUIT 		= 4'd7;
 		  
     always @(*)
     begin
         case (current_state)
-            NO_DRAW: next_state = go ? LOAD_NUM_SUIT : NO_DRAW;
-				LOAD_NUM_SUIT : next_state = DRAW_NUM;
-            DRAW_BLANK : next_state = WAIT_BLANK;
-				WAIT_BLANK : next_state = blankDone ? DRAW_NUM : WAIT_BLANK;
-				DRAW_NUM : next_state = WAIT_NUM;
-				WAIT_NUM : next_state = numDone ? DRAW_SUIT : WAIT_NUM;
-				DRAW_SUIT : next_state = WAIT_SUIT;
-				WAIT_SUIT : next_state = suitDone ? NO_DRAW : WAIT_SUIT;
+            NO_DRAW 			: next_state = go ? LOAD_NUM_SUIT : NO_DRAW;
+				LOAD_NUM_SUIT 	: next_state = DRAW_BLANK;
+            DRAW_BLANK 		: next_state = WAIT_BLANK;
+				WAIT_BLANK		: next_state = blankDone ? DRAW_NUM : WAIT_BLANK;
+				DRAW_NUM 		: next_state = WAIT_NUM;
+				WAIT_NUM			: next_state = numDone ? DRAW_SUIT : WAIT_NUM;
+				DRAW_SUIT 		: next_state = WAIT_SUIT;
+				WAIT_SUIT		: next_state = suitDone ? NO_DRAW : WAIT_SUIT;
             default: next_state = NO_DRAW;
        endcase
     end
 
     always @(posedge clock)
     begin
-		  loadNumSuit = 1'b0;
-		  drawBlank = 1'b0;
-		  drawNum = 1'b0;
-		  drawSuit = 1'b0;
-        plot = 1'b0;
+		loadNumSuit = 1'b0;
+		drawBlank = 1'b0;
+		drawNum = 1'b0;
+		drawSuit = 1'b0;
+      plot = 1'b0;
 
-        case (current_state)
-				LOAD_NUM_SUIT: loadNumSuit = 1'b1;
-            DRAW_BLANK: begin
-					drawBlank = 1'b1;
-					plot = 1'b1;
-				end
-				WAIT_BLANK: begin
-					drawBlank = 1'b1;
-					plot = 1'b1;
-				end
-				DRAW_NUM: begin
-					drawNum = 1'b1;
-					plot = 1'b1;
-				end
-				WAIT_NUM: begin
-					drawNum = 1'b1;
-					plot = 1'b1;
-				end
-				DRAW_SUIT: begin
-					drawSuit = 1'b1;
-					plot = 1'b1;
-				end
-				WAIT_SUIT: begin
-					drawSuit = 1'b1;
-					plot = 1'b1;
-				end
-        endcase
+      case (current_state)
+			LOAD_NUM_SUIT: loadNumSuit = 1'b1;
+         DRAW_BLANK, WAIT_BLANK: begin
+				drawBlank <= 1'b1;
+				plot <= 1'b1;
+			end
+			DRAW_NUM, WAIT_NUM: begin
+				drawNum <= 1'b1;
+				plot <= 1'b1;
+			end
+			DRAW_SUIT, WAIT_SUIT: begin
+				drawSuit <= 1'b1;
+				plot <= 1'b1;
+			end
+       endcase
     end
 
     // current_state registers
@@ -394,7 +396,7 @@ module control(
 			current_state <= NO_DRAW;
 		else
 			current_state <= next_state;
-		end
+	end
 endmodule
 
 module Counter(
@@ -422,7 +424,7 @@ module TFlipFlop(t, q, clock, resetN);
 	output reg q;
 	always @ (posedge clock, negedge resetN) begin
 		if (resetN == 1'b0)
-			q <= 0;
+			q <= 1'b0;
 		else if (t == 1'b1)
 			q <= 1'b1 - q;
 	end
@@ -457,177 +459,157 @@ endmodule
 module drawBlankCard(
 	input resetN,
 	input clock,
+
+	input start,
+
 	input [7:0] x,
 	input [6:0] y,
+	output reg [7:0] xOut,
+	output reg [6:0] yOut,
+	output reg [2:0] cOut,
+
+	output reg done
+	);
+	reg [7:0] xReg, xOffset;
+	reg [6:0] yReg, yOffset;
+
+	reg [1:0] current_state, next_state;
+
+	localparam
+		NO_DRAW 	= 2'd0,
+		DRAW		= 2'd1;
+
+	always@(*) begin
+		case (current_state)
+			NO_DRAW 		: next_state = start ? DRAW : NO_DRAW;
+			DRAW			: next_state = done ? NO_DRAW : DRAW;
+		endcase
+	end
+
+	always@(posedge clock) begin
+		if (start) begin
+			xReg <= x;
+			yReg <= y;
+		end
+		
+		case (current_state)
+			NO_DRAW	: begin
+				xOffset <= 8'b0;
+				yOffset <= 7'b0;
+				xReg <= 8'b0;
+				yReg <= 7'b0;
+				xOut <= 8'b0;
+				yOut <= 7'b0;
+				cOut <= 3'b0;
+				done <= 1'b1;
+			end
+			DRAW: begin
+				done <= 1'b0;
+				if (xOffset < 8'd23)
+					xOffset <= xOffset + 1'b1;
+				else if (yOffset < 8'd39) begin
+					yOffset <= yOffset + 1'b1;
+					xOffset <= 8'b0;
+				end else
+					done <= 1'b1;
+
+				xOut <= xReg + xOffset;
+				yOut <= yReg + yOffset;
+				cOut <= 3'b111;
+			end
+		endcase
+	end
+
+	always@(posedge clock) begin
+		if (!resetN)
+			current_state <= NO_DRAW;
+		else
+			current_state <= next_state;
+	end
+endmodule
+
+module drawNumSuit(
+	input resetN,
+	input clock,
+	
+	input start,
+
+	input [7:0] x,
+	input [6:0] y,
+	
+	input [767:0] data,
+
 	output reg [7:0] xOut,
 	output reg [6:0] yOut,
 	output reg [2:0] cOut,
 	output reg done
 	);
-	reg [7:0] xOffset;
-	reg [6:0] yOffset;
+
+	reg [7:0] xReg, xOffset;
+	reg [6:0] yReg, yOffset;
+	reg [767:0] dataReg;
+
+	reg [2:0] current_state, next_state;
+	
+	localparam
+		NO_DRAW 		= 3'd0,
+		LOAD			= 3'd1,
+		DRAW			= 3'd2;
+
+	always@(*) begin
+		case (current_state)
+			NO_DRAW 			: next_state = start ? LOAD : NO_DRAW;
+			LOAD				: next_state = DRAW;
+			DRAW				: next_state = done ? NO_DRAW : DRAW;
+			default			: next_state = NO_DRAW;
+		endcase
+	end
 	
 	always@(posedge clock) begin
-		if (!resetN) begin
-			xOffset <= 8'b0;
-			yOffset <= 7'b0;
-			xOut <= 8'b0;
-			yOut <= 7'b0;
-			cOut <= 3'b0;
-			done <= 1'b0;
-		end else if (xOffset < 8'd23)
-			xOffset <= xOffset + 1'b1;
-		else if (yOffset < 8'd39) begin
-			yOffset <= yOffset + 1'b1;
-			xOffset <= 8'b0;
-		end else
-			done <= 1'b1;
-		
-		xOut <= x + xOffset;
-		yOut <= y + yOffset;
-		cOut <= 3'b111;
+		case (current_state)
+			NO_DRAW: begin
+				if (!resetN) begin
+					dataReg <= 768'b0;
+					xReg <= 8'b0;
+					yReg <= 7'b0;
+					xOut <= 8'b0;
+					yOut <= 7'b0;
+					cOut <= 2'b0;
+					xOffset <= 8'b0;
+					yOffset <= 7'b0;
+					done <= 1'b1;
+				end
+			end
+
+			LOAD:	begin
+						done <= 1'b0;
+						dataReg <= data;
+						xReg <= x;
+						yReg <= y;
+					end
+			DRAW: begin
+				if (xOffset < 8'd15) begin
+					xOffset <= xOffset + 1'b1;
+					dataReg <= dataReg << 3;
+				end else if (yOffset < 8'd15) begin
+					yOffset <= yOffset + 1'b1;
+					xOffset <= 8'b0;
+					dataReg <= dataReg << 3;
+				end else begin
+					done <= 1'b1;
+				end
+				
+				xOut <= xReg + xOffset;
+				yOut <= yReg + yOffset;
+				cOut <= dataReg [767:765];
+			end
+		endcase
+	end
+
+	always@(posedge clock) begin
+		if (!resetN)
+			current_state <= NO_DRAW;
+		else
+			current_state <= next_state;
 	end
 endmodule
-
-module drawNum(
-	input resetN,
-	input clock,
-	input [7:0] x,
-	input [6:0] y,
-	input [767:0] in_data,
-	output reg [7:0] xOut,
-	output reg [6:0] yOut,
-	output reg [2:0] cOut,
-	output reg done,
-	output reg [767:0] data
-	);
-	reg [7:0] xOffset;
-	reg [6:0] yOffset;
-	
-	always@(posedge clock) begin
-		if (!resetN) begin
-			xOffset <= 8'b0;
-			yOffset <= 7'b0;
-			xOut <= 8'b0;
-			yOut <= 7'b0;
-			data <= in_data;
-			done <= 1'b0;
-		end else if (xOffset < 8'd15) begin
-			xOffset <= xOffset + 1'b1;
-			data <= data << 3;
-		end else if (yOffset < 8'd15) begin
-			yOffset <= yOffset + 1'b1;
-			xOffset <= 8'b0;
-			data <= data << 3;
-		end else
-			done <= 1'b1;
-		
-		xOut <= x + xOffset;
-		yOut <= y + yOffset;
-		cOut <= data[767:765];
-	end
-endmodule
-
-/**module drawCard2(
-	input resetN,
-	input clock,
-	input [7:0] x,
-	input [6:0] y,
-	input [3:0] cardNum,
-	input [1:0] cardSuit,
-	output reg [7:0] xOut,
-	output reg [6:0] yOut,
-	output reg [2:0] cOut,
-	output done
-	);	
-	wire [767:0] ace;
-	wire [767:0] two;
-	assign two = 768'hFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFC0FFFFFFFFFE071FFFFFFFFE3F1FFFFFFFFFF8FFFFFFFFFE00FFFFFFFFFE3FFFFFFFFFFE001FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF;
-	wire [767:0] three;
-	wire [767:0] four;
-	wire [767:0] five;
-	wire [767:0] six;
-	wire [767:0] seven;
-	wire [767:0] eight;
-	wire [767:0] nine;
-	wire [767:0] ten;
-	wire [767:0] jack;
-	wire [767:0] queen;
-	wire [767:0] king;
-	
-	wire [767:0] spade;
-	wire [767:0] diamond;
-	wire [767:0] club;
-	wire [767:0] heart;
-	assign heart = 768'hFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF03003FFFFFF8000007FFFFF8000007FFFFF8000007FFFFF8000007FFFFFF00003FFFFFFFE001FFFFFFFFFC7FFFFFFFFFFFBFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF;
-
-	reg [767:0] num;
-	reg [767:0] suit;
-	reg [767:0] in_data;
-	
-	reg [7:0] x_loc;
-	reg [6:0] y_loc;
-	reg resetBG_n;
-	reg resetNum_n;
-	reg [1:0] current_state;
-	
-	wire mini_done;
-	
-	always@(posedge clock) begin
-		case (cardNum)
-			4'd1: num <= ace;
-			4'd2: num <= two;
-			4'd3: num <= three;
-			4'd4: num <= four;
-			4'd5: num <= five;
-			4'd6: num <= six;
-			4'd7: num <= seven;
-			4'd8: num <= eight;
-			4'd9: num <= nine;
-			4'd10: num <= ten;
-			4'd11: num <= jack;
-			4'd12: num <= queen;
-			4'd13: num <= king;
-			default: num <= ace;
-		endcase
-		
-		case (cardSuit)
-			2'd0: suit <= spade;
-			2'd1: suit <= diamond;
-			2'd2: suit <= club;
-			2'd3: suit <= heart;
-			default: suit <= spade;
-		endcase
-		
-		if(!resetN) begin
-			resetBG_n <= 1;
-			resetNum_n <= 1;
-			current_state <= 0;
-		end
-	end
-	
-	drawNum dc(
-		.resetN(resetNum_n),
-		.clock(clock),
-		.x(x),
-		.y(7'd10),
-		.in_data(in_data),
-		.xOut(x[7:0]),
-		.yOut(y[6:0]),
-		.cOut(colour[2:0]),
-		.done(mini_done),
-		.data(data)
-	);
-		
-	drawBlankCard bc(
-		.resetN(resetBG_n),
-		.clock(clock),
-		.x(x),
-		.y(y),
-		.xOut(x[7:0]),
-		.yOut(y[6:0]),
-		.cOut(colour[2:0]),
-		.done(mini_done),
-		.data(data)
-	);
-endmodule**/
